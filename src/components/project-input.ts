08928dfa485015b4e30a8fd123821aa9f9b163b6
dedecanton <andrecanton18@gmail.com>
2022-01-27 14:13:44 -0300

    project completed

---
+++ src/components/project-input.ts
@@ -0,0 +1,81 @@
+import { Component } from "./base-component.js";
+import { Validatable, validate } from "../util/validation.js";
+import { autobind } from "../decorators/autobind.js";
+import { projectSate } from "../state/project.js";
+
+// Project Input Class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    super("project-input", "app", true, "user-input");
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+  }
+
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  renderContent(): void {}
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enetredTitle = this.titleInputElement.value;
+    const enetredDescription = this.descriptionInputElement.value;
+    const enetredPeople = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enetredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enetredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enetredPeople,
+      required: true,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input");
+      return;
+    } else {
+      return [enetredTitle, enetredDescription, +enetredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      projectSate.addProject(title, desc, people);
+      this.clearInputs();
+    }
+  }
+}
